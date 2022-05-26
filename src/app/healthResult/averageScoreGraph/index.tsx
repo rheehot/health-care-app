533bdc2c50261ac042a5c8da8bc1ec515f5c7d67
daseuls <dls2tmfs2@naver.com>
2022-05-26 22:41:56 +0900

    feat: 폴더, 컴포넌트 생성

---
+++ src/app/healthResult/averageScoreGraph/index.tsx
@@ -0,0 +1,5 @@
+const AverageScoreGraph = () => {
+  return <div>스코어</div>;
+};
+
+export default AverageScoreGraph;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import styles from './app.module.scss';
+import AverageScoreGraph from './healthResult/averageScoreGraph';
 
 const App = () => {
   return (
     <div className={styles.appWrapper}>
       <div className={styles.app}>
         <main className={styles.scrollArea} />
+        <AverageScoreGraph />
       </div>
     </div>
   );
