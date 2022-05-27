096d5d4c0b8ae03042d17f12465a9781d7b127ad
daseuls <dls2tmfs2@naver.com>
2022-05-27 23:42:23 +0900

    feat: 메인 pull

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,4 @@
 import styles from './app.module.scss';
-import AverageScoreGraph from './healthResult/averageScoreGraph';
 import MyHealth from './myHealth';
 
 const App = () => {
@@ -9,7 +8,6 @@ const App = () => {
         <h1 className={styles.title}>마이헬스</h1>
         <main className={styles.scrollArea}>
           <MyHealth />
-          <AverageScoreGraph />
           <div style={{ height: '200px', backgroundColor: 'yellow' }}>다른컴포넌트</div>
           <div style={{ height: '200px', backgroundColor: 'red' }}>다른컴포넌트</div>
         </main>
