1288679d1a063609927d9110ed6071a797a5201e
홍선영 <allie@hongseon-yeong-ui-MacBookPro.local>
2022-05-27 11:03:43 +0900

    feat: 건강점수차트 추가

--- src/app/myHealth/index.tsx
+++ src/app/myHealth/index.tsx
@@ -1,9 +1,18 @@
 import { AiOutlineInfoCircle } from 'react-icons/ai';
+import { VictoryPie } from 'victory';
+
+import formatDate from 'utils/formatDate';
 import response from '../../data/response.json';
 import styles from './myHealth.module.scss';
 
 const MyHealth = () => {
+  const MAX_HEALTH_SCORE = 1000;
+
   const { age, sex, resHeight } = response.wxcResultMap.paramMap;
+  const { healthScore, healthDate } = response.userInfo;
+
+  const healthScorePortion = (healthScore / MAX_HEALTH_SCORE) * 100;
+  const remainder = 100 - healthScorePortion;
 
   return (
     <section>
@@ -13,7 +22,28 @@ const MyHealth = () => {
           <span>김헬스님의 건강점수</span>
           <AiOutlineInfoCircle className={styles.infoIcon} size='18' color='#7d7d79' />
         </div>
-        <div>chart</div>
+        <div className={styles.score}>
+          {healthScore}
+          <span>점</span>
+        </div>
+        <VictoryPie
+          data={[
+            { x: 1, y: remainder, label: ' ' },
+            { x: 2, y: healthScorePortion, label: ' ' },
+          ]}
+          startAngle={120}
+          endAngle={-120}
+          animate={{
+            duration: 2000,
+            onLoad: { duration: 2000 },
+          }}
+          innerRadius={180}
+          colorScale={['#d9d9d9', '#f7c505']}
+        />
+        <div className={styles.date}>{formatDate(healthDate)}</div>
+        <button type='button' className={styles.resultButton}>
+          건강검진결과 가져오기 {'>'}
+        </button>
         <dl className={styles.basicInfo}>
           <dt>기본정보</dt>
           <div>
@@ -22,6 +52,10 @@ const MyHealth = () => {
             <dd>{resHeight}cm</dd>
           </div>
         </dl>
+        <div className={styles.analysis}>
+          <span>건강점수 분석 결과</span>
+          <button type='button'>결과 자세히 보기 {'>'}</button>
+        </div>
       </div>
     </section>
   );
