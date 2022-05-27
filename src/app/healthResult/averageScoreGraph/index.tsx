136f0f5c57654e2ee176640340c99a71679afa79
daseuls <dls2tmfs2@naver.com>
2022-05-27 15:26:54 +0900

    feat: 텍스트 수정

--- src/app/healthResult/averageScoreGraph/index.tsx
+++ src/app/healthResult/averageScoreGraph/index.tsx
@@ -1,4 +1,12 @@
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryLine,
+  VictoryScatter,
+  VictoryLabel,
+} from 'victory';
 import data from '../../../data/response.json';
 import styles from './averageScoreGraph.module.scss';
 import cn from 'classnames';
@@ -9,8 +17,8 @@ const AverageScoreGraph = () => {
   const { userInfo, wxcResultMap } = data;
 
   const GRAPTH_DATA = [
-    { user: '나', score: userInfo.healthScore, label: userInfo.healthScore },
-    { user: '30대 남성', score: wxcResultMap.hscore_peer, label: wxcResultMap.hscore_peer },
+    { user: '나', score: userInfo.healthScore },
+    { user: '30대 남성', score: wxcResultMap.hscore_peer },
   ];
 
   const isHigherMyScore = userInfo.healthScore > wxcResultMap.hscore_peer;
@@ -19,17 +27,46 @@ const AverageScoreGraph = () => {
 
   return (
     <div className={styles.averageWrapper}>
-      <p>30대 남성 평균 점수보다</p>
-      <p className={cn(styles.score, { [styles.highScoreColor]: isHigherMyScore })}>
-        {isHigherMyScore ? scoreDifference : -scoreDifference}점 {isHigherMyScore ? '높아요' : '낮아요'}
-      </p>
-      <p>
-        {isHighRank ? '상위' : '하위'} {100 - wxcResultMap.hscorePercent}%
-      </p>
-      <VictoryChart domainPadding={100} theme={VictoryTheme.material}>
-        <VictoryAxis dependentAxis />
+      <div className={styles.descriptionWrapper}>
+        <div className={styles.scoreTextWrapper}>
+          <p>30대 남성 평균 점수보다</p>
+          <p className={cn(styles.score, { [styles.highScoreColor]: isHigherMyScore })}>
+            {isHigherMyScore ? scoreDifference : -scoreDifference}점 {isHigherMyScore ? '높아요.' : '낮아요.'}
+          </p>
+        </div>
+        <p className={styles.rankText}>
+          {isHighRank ? '상위' : '하위'} {100 - wxcResultMap.hscorePercent}%
+        </p>
+      </div>
+      <VictoryChart domainPadding={70}>
         <VictoryAxis />
-        <VictoryBar data={GRAPTH_DATA} x='user' y='score' />
+        <VictoryBar
+          height={500}
+          barWidth={50}
+          animate={{
+            duration: 1000,
+            onLoad: { duration: 1000 },
+          }}
+          style={{
+            data: { fill: '#ffd202' },
+            labels: { fill: '#666666' },
+          }}
+          data={GRAPTH_DATA}
+          x='user'
+          y='score'
+          labels={GRAPTH_DATA.map((el) => el.score)}
+        />
+        <VictoryLine
+          data={GRAPTH_DATA}
+          animate={{
+            duration: 1000,
+            onLoad: { duration: 1000 },
+          }}
+          style={{ data: { fill: '#666666' } }}
+          x='user'
+          y='score'
+        />
+        <VictoryScatter style={{ data: { fill: '#666666' } }} data={GRAPTH_DATA} x='user' y='score' />
       </VictoryChart>
     </div>
   );
