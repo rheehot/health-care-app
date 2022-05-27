465829a5489d1cf3b9fedb322e447addb3ccfade
Seonmi Jung <devkatej927@gmail.com>
2022-05-27 23:54:52 +0900

    style: 그래프 위치 조정

--- src/app/healthResult/costPredictionGraph/index.tsx
+++ src/app/healthResult/costPredictionGraph/index.tsx
@@ -1,24 +1,24 @@
+import { useState } from 'react';
 import { VictoryChart, VictoryAxis, VictoryBar, VictoryScatter, VictoryLine, VictoryLabel } from 'victory';
 import Big from 'big.js';
+
 import response from 'data/response.json';
 import { convertToData, compareCost, GRAPH_OPTIONS } from './_shared';
 import { COLORS } from '../_shared';
-
-import { useState } from 'react';
 
 import cn from 'classnames';
 import styles from './costPredictionGraph.module.scss';
 
 const CostPredictionGraph = () => {
   const [isShowScatter, setIsShowScatter] = useState(false);
+  setTimeout(() => setIsShowScatter(true), 1300);
+
   const { medi, mediDy } = response.wxcResultMap;
   const currentCost = new Big(medi);
   const after10yrsCost = new Big(mediDy[mediDy.length - 1]).toNumber();
 
   const data = convertToData(currentCost.toNumber(), after10yrsCost);
   const { txt, costStatus } = compareCost(currentCost, after10yrsCost);
-
-  setTimeout(() => setIsShowScatter(true), 1300);
 
   return (
     <article>
@@ -28,7 +28,7 @@ const CostPredictionGraph = () => {
         <mark className={cn(styles.comparedResult, styles[costStatus])}>{txt[1]}</mark>
       </h3>
       <div className={styles.graphWrapper}>
-        <VictoryChart domainPadding={20}>
+        <VictoryChart domainPadding={{ x: [70, 0] }}>
           <VictoryAxis {...GRAPH_OPTIONS.axis} />
           <VictoryBar data={data} style={{ data: { fill: ({ datum }) => datum.fill.bar } }} {...GRAPH_OPTIONS.bar} />
           <VictoryLine data={data} {...GRAPH_OPTIONS.line} />
