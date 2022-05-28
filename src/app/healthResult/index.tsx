4732552f0ddb73b6fa04e7f75c10580a07281b3d
WOO SEONG LEE <42796944+leewooseong@users.noreply.github.com>
2022-05-28 10:06:18 +0900

    feat : index.tsx 그래프 추가

--- src/app/healthResult/index.tsx
+++ src/app/healthResult/index.tsx
@@ -1,10 +1,12 @@
 import AverageScoreGraph from './averageScoreGraph';
 import PredictGraph from './predictGraph';
 import CostPredictionGraph from './costPredictionGraph';
+import CompareLastYear from './compareLastYear';
 
 const HealthResult = () => {
   return (
     <section>
+      <CompareLastYear />
       <AverageScoreGraph />
       <PredictGraph />
       <CostPredictionGraph />
